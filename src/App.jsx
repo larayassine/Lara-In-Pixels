48f20493011435982490a6f4054ce52bd237657e
Lara Yassine <laraiyassine@gmail.com>
2025-01-23 17:44:12 -0500

    added skills and about

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,16 @@
-
-import styles from './App.module.css';
-import { About } from './components/About/About';
-import { Experience } from './components/Experience/Experience';
-import { Hero } from './components/Hero/Hero';
-import { Navbar } from './components/navbar/navbar';
-import { Projects } from './components/Projects/Projects';
+import styles from "./App.module.css";
+import { About } from "./components/About/About";
+import { Experience } from "./components/Experience/Experience";
+import { Hero } from "./components/Hero/Hero";
+import { Navbar } from "./components/navbar/navbar";
 function App() {
-
-
   return (
     <div className={styles.App}>
       <Navbar></Navbar>
       <Hero></Hero>
       <About></About>
       <Experience></Experience>
-      <Projects></Projects>
     </div>
   );
-   
-  
 }
 export default App;
