dd30b7e2cd959ac5564851e686d732115a84b31f
Lara Yassine <laraiyassine@gmail.com>
2025-03-25 23:40:27 -0400

    Updated Navbar file path

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import { About } from "./components/About/About";
 import { Contact } from "./components/Contact/Contact";
 import { Experience } from "./components/Experience/Experience";
 import { Hero } from "./components/Hero/Hero";
-import { Navbar } from "./components/navbar/navbar";
+import { Navbar } from "./components/Navbar/Navbar";
+
 
 import { Projects } from "./components/Projects/Projects";
 function App() {
